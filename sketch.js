9a62681dba2b9cbf91d60c1d3c080b10eddba635
pattydunc4444 <patrickduncan44444@gmail.com>
2025-05-30 13:28:04 -0400

    version 4 with floor

--- sketch.js
+++ sketch.js
@@ -5,143 +5,133 @@
 
 var floorArray = [];
 var grass;
-var z,x,y;
+var z, x, y;
 var bricks;
-var wallArray=[];
+var wallArray = [];
 var lastGoodX = 0;
 var lastGoodY = 0;
 var lastGoodZ = 0;
 var camAngle;
 let bCam;
-var dx,dz;
+var dx, dz;
 let graphics, graphics2;
 
-function preload(){
-  bricks=loadImage("brick.jpg");
-  grass = loadImage("tileable-img_0062-verydark.png"); // 
+function preload() {
+  bricks = loadImage("brick.jpg");
+  grass = loadImage("thumbnail.jpg"); // 
 }
 
 
 function setup() {
-  createCanvas(windowWidth, windowHeight,WEBGL);
-  y=0;
-  x=0;
-  z=0;
-   floorArray.push(new floor(0, 40, 0, 2000, 40, 2000)); 
+  createCanvas(windowWidth, windowHeight, WEBGL);
+  y = 0;
+  x = 0;
+  z = 0;
+
   //sign graphics
-graphics = createGraphics(800,400);
-  graphics2 = createGraphics(420,400);
- 
-//bcam is a birds eye view that helps when placing blocks  
+  graphics = createGraphics(800, 400);
+  graphics2 = createGraphics(420, 400);
+
+  //bcam is a birds eye view that helps when placing blocks  
   bCam = createCamera();
-    bCam.setPosition(0,-3000,200);
-//cam is the player camera
-   cam = createCamera();
-  cam.setPosition(400,0,800);
-  
-  
+  bCam.setPosition(0, -3000, 200);
+  //cam is the player camera
+  cam = createCamera();
+  cam.setPosition(400, 0, 800);
+
+
   //maze creation
   //wallArray.push(new wall(0,0,0));
-  wallArray.push(new wall(400,0,400));
-  for(let i=0;i<3;i++){
-    wallArray.push(new wall(400-400*i,0,-1200,1));
-    wallArray.push(new wall(400-4*i,0,-800));
+  wallArray.push(new wall(400, 0, 400));
+  for (let i = 0; i < 3; i++) {
+    wallArray.push(new wall(400 - 400 * i, 0, -1200, 1));
+    wallArray.push(new wall(400 - 4 * i, 0, -800));
   }
- // ...existing code...
-var floorArray = []; // add this near wallArray
-
+  floorArray.push(new floor(200, 225, -200, 1000, 40, 2000)); // Add this line
+}
 
 
 
 function draw() {
- background(120);
+  background(120);
   for (let i = 0; i < floorArray.length; i++) {
     floorArray[i].display();
   }
-  //start sign creating a sign for the game start
-  push();
 
-  pop();
-  
-  //creating a sign for the game end
-  push();
- 
-  pop();
+  //maze building camera
+  bCam.lookAt(0, 0, 0);
+  //setCamera(bCam);
 
- //maze building camera
-   bCam.lookAt(0,0,0);
-   //setCamera(bCam);
-  
- 
-frameRate(60);
-    x =0;
-    z =0;
-  camAngle=0;
+
+  // frameRate(60);
+  x = 0;
+  z = 0;
+  camAngle = 0;
   //player "flash light"
   pointLight(200, 200, 200, cam.eyeX, -200, cam.eyeZ);
   ambientLight(5);
-  
+
   //creating the gem
   push();
-  
+
   pop();
- 
+
 
   var anyTouching = false;
   //displays all blocks in an array
- for (let i=0; i<wallArray.length; i++){
-   wallArray[i].display();
-   //tests if the wall class is touching the camera
-   if(wallArray[i].touching()){
-         anyTouching=true;
-   }
- }
-  //saves the last "safe" player location to return the player to.
-  if(anyTouching==false){
-
-    lastGoodX=(cam.eyeX);
-    lastGoodY = cam.eyeY;
-    lastGoodZ=(cam.eyeZ);
-      //console.log(lastGoodX+" "+lastGoodZ);
+  for (let i = 0; i < wallArray.length; i++) {
+    wallArray[i].display();
+    //tests if the wall class is touching the camera
+    if (wallArray[i].touching()) {
+      anyTouching = true;
+    }
   }
-  
+  //saves the last "safe" player location to return the player to.
+  if (anyTouching == false) {
 
-//wasd controls for strafing
-    if(keyIsDown(83)){
-     z=10;      
-     }
-  
-   if(keyIsDown(87)){
-     z=-10;              
-    }
-  
-   if(keyIsDown(65)){
-     x=-10;                           
-   }
-  
-   if(keyIsDown(68)){
-     x=10;                             
-    }
+    lastGoodX = (cam.eyeX);
+    lastGoodY = cam.eyeY;
+    lastGoodZ = (cam.eyeZ);
+    //console.log(lastGoodX+" "+lastGoodZ);
+  }
 
- //puts player back to last place not touching a wall 
-if(anyTouching==true){
-      cam.setPosition(lastGoodX,lastGoodY,lastGoodZ);
-      //console.log("touching");
-      
-      //set to last safe position
 
-    }
-//looking side to side controls  
-    if(keyIsDown(37)){
-     camAngle=0.05;                             
-    }
-     if(keyIsDown(39)){
-     camAngle=-0.05;                             
-    }
+  //wasd controls for strafing
+  if (keyIsDown(83)) {
+    z = 10;
+  }
+
+  if (keyIsDown(87)) {
+    z = -10;
+  }
+
+  if (keyIsDown(65)) {
+    x = -10;
+  }
+
+  if (keyIsDown(68)) {
+    x = 10;
+  }
+
+  //puts player back to last place not touching a wall 
+  if (anyTouching == true) {
+    cam.setPosition(lastGoodX, lastGoodY, lastGoodZ);
+    //console.log("touching");
+
+    //set to last safe position
+
+  }
+  //looking side to side controls  
+  if (keyIsDown(37)) {
+    camAngle = 0.05;
+  }
+  if (keyIsDown(39)) {
+    camAngle = -0.05;
+  }
   //mouse would often leave the player stuck
-    //cam.pan(movedX*-0.01);
-    cam.pan(camAngle);
+  //cam.pan(movedX*-0.01);
+  cam.pan(camAngle);
   //cam.tilt(movedY*0.01);
-    cam.move(x,0,z);
- // console.log(cam.eyeZ);
+  cam.move(x, 0, z);
+  // console.log(cam.eyeZ);
 }
