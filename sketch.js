c2fba78e01e69f5f055372ae327c8065f211ab0b
pattydunc4444 <patrickduncan44444@gmail.com>
2025-06-03 13:23:21 -0400

    part 9 with dumb enemy

--- sketch.js
+++ sketch.js
@@ -18,6 +18,7 @@ let graphics, graphics2;
 let skyboxImgs = {};
 let crosshairGfx;
 let gunImg;
+let opponent;
 
 function preload() {
   bricks = loadImage("brick.jpg");
@@ -65,6 +66,7 @@ function setup() {
   floorArray.push(new FloorConcrete(0, 230, 0, 7000, 40, 7000)); // Concrete floor, 5 units below, fits skybox
   crosshairGfx = createGraphics(windowWidth, windowHeight);
   crosshairGfx.clear();
+  opponent = new Opponent(0, 230, -1000); // Y=230 matches your concrete floor
 }
 
 
@@ -128,6 +130,12 @@ function draw() {
 
   for (let i = 0; i < floorArray.length; i++) {
     floorArray[i].display();
+  }
+
+  // Draw the opponent in 3D world space
+  if (opponent) {
+    opponent.move();
+    opponent.display();
   }
 
   //maze building camera
@@ -251,5 +259,90 @@ class FloorConcrete {
     texture(concrete);
     box(this.w, this.h, this.d);
     pop();
+  }
+}
+
+class Opponent {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+    this.size = 100;
+    this.alive = true;
+    this.dirX = random([-1, 1]);
+    this.dirZ = random([-1, 1]);
+    this.speed = 2;
+  }
+
+  move() {
+    if (!this.alive) return;
+
+    // Try to move
+    let nextX = this.x + this.dirX * this.speed;
+    let nextZ = this.z + this.dirZ * this.speed;
+
+    // Check for wall collision
+    let willCollide = false;
+    for (let i = 0; i < wallArray.length; i++) {
+      let wall = wallArray[i];
+      // Simple AABB collision
+      if (
+        nextX + this.size/2 > wall.x - wall.w/2 &&
+        nextX - this.size/2 < wall.x + wall.w/2 &&
+        this.y + this.size > wall.y - wall.h/2 &&
+        this.y < wall.y + wall.h/2 &&
+        nextZ + this.size/2 > wall.z - wall.d/2 &&
+        nextZ - this.size/2 < wall.z + wall.d/2
+      ) {
+        willCollide = true;
+        break;
+      }
+    }
+
+    // If collision, pick a new random direction
+    if (willCollide) {
+      this.dirX = random([-1, 0, 1]);
+      this.dirZ = random([-1, 0, 1]);
+      // Don't let both be zero
+      if (this.dirX === 0 && this.dirZ === 0) this.dirX = 1;
+    } else {
+      // Move if no collision
+      this.x = nextX;
+      this.z = nextZ;
+    }
+  }
+
+  display() {
+    if (!this.alive) return;
+    push();
+    translate(this.x, this.y, this.z);
+    fill(255, 0, 0);
+    box(this.size, this.size * 2, this.size);
+    pop();
+  }
+
+  isTargeted(cam) {
+    let dx = this.x - cam.eyeX;
+    let dy = this.y - cam.eyeY;
+    let dz = this.z - cam.eyeZ;
+    let distToOpponent = Math.sqrt(dx*dx + dy*dy + dz*dz);
+
+    let cx = cam.centerX - cam.eyeX;
+    let cy = cam.centerY - cam.eyeY;
+    let cz = cam.centerZ - cam.eyeZ;
+    let camMag = Math.sqrt(cx*cx + cy*cy + cz*cz);
+
+    cx /= camMag; cy /= camMag; cz /= camMag;
+    dx /= distToOpponent; dy /= distToOpponent; dz /= distToOpponent;
+
+    let dot = cx*dx + cy*dy + cz*dz;
+    return dot > 0.995 && distToOpponent < 2000;
+  }
+}
+
+function keyPressed() {
+  // Press SPACE to "shoot"
+  if (key === ' ' && opponent && opponent.alive && opponent.isTargeted(cam)) {
+    opponent.alive = false;
   }
 }
